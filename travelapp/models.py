cb1320e4646c63f4419653736fbdc1b457ca162f
HibacMeh <hibacm911@gmail.com>
2023-03-28 20:07:33 -0700

    create models for database

--- travelapp/models.py
+++ travelapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Places(models.Model):
+    name = models.CharField(max_length=250)
+    img = models.ImageField(upload_to="pics")
+    desc = models.TextField()
+
+    
