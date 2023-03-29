ba639b9ad74df259e336cf0adbd09127b362bba5
HibacMeh <hibacm911@gmail.com>
2023-03-28 22:18:07 -0700

    adding jinja

--- travelapp/views.py
+++ travelapp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
+from . models import Places
+
 # Create your views here
 
 def demo(request) :
-    return render(request, "index.html")
+
+    obj = Places.objects.all
+    return render(request, "index.html", {'result': obj})
 
 # def result(request):
 #     return render(request, "result.html")
