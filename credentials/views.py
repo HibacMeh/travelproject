c8361659b48823c0e76e30579b639ff686e252f8
HibacMeh <hibacm911@gmail.com>
2023-03-31 06:30:06 +0530

    sanitation

--- credentials/views.py
+++ credentials/views.py
@@ -4,14 +4,22 @@ from django.shortcuts import render
 
 # Create your views here.
 def cred (request):
+    #the auth table has been already craeted by django 
+    
     if request.method == 'POST':
       firstname = request.POST['firstname']
       lastname= request.POST['lastname']
       email = request.POST['email']
       password = request.POST['password']
       cpassword= request.POST['confirm_password']
+      
+      if password==cpassword:
+         user = UserString.objects.create_user(firstname= firstname, lastname=lastname,email=email,password=password)
+         user.save();
+         print("user has been saved")
 
-      user = UserString.objects.create_user(firstname= firstname, lastname=lastname,email=email,password=password)
+      else:
+         print("password not matching")
 
       
 
